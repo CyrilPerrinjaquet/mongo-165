7ecd5ed3a44b0ed2b15f8845642909576e2a3bb4
Nicolas Loison <nicolasloison13@icloud.com>
2024-05-23 14:52:01 +0200

    add fetch and randomData

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+
 function App(): JSX.Element {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("");
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const getRandomData = () => {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    return data[randomIndex];
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <h1 className="text-3xl font-bold underline">Module 165 - MongoDB</h1>
+      <h2>Data aléatoire </h2>
+      {data.length > 0 && (
+        <div>
+          <h2>Random Data:</h2>
+          <p>{getRandomData()}</p>
+        </div>
+      )}
     </div>
   );
 }
